e04b5021f7f366ba66014f2331d38b6fb92a81ce
mepravindesai <Pd29desai@gmail.com>
2018-04-08 16:30:13 +0530

    small change

--- src/HelloGIT.java
+++ src/HelloGIT.java
@@ -3,7 +3,7 @@ public class HelloGIT {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	System.out.println("pravin how is going on");
+	System.out.println("pravin ow is going on");
 		
 	}
 
